b1befa4834005edb6e426b28d40a02d4aefcd03f
hadzija7 <hadzijasale@gmail.com>
2023-05-13 11:05:46 +0200

    wip osmo-secret bridge

--- src/dex/shade/osmoSecretBridge.ts
+++ src/dex/shade/osmoSecretBridge.ts
@@ -7,8 +7,8 @@ import { getOfflineSignerAmino as getOfflineSigner } from 'cosmjs-utils';
 import { cosmos, getSigningOsmosisClient, ibc } from "osmojs";
 import { MsgTransfer } from "osmojs/types/codegen/ibc/applications/transfer/v1/tx";
 import { MsgSend } from "osmojs/types/codegen/cosmos/bank/v1beta1/tx";
-import { SigningStargateClient } from "@cosmjs/stargate"
-import { coin, coins } from "@cosmjs/proto-signing";
+import { SigningStargateClient, SigningStargateClientOptions } from "@cosmjs/stargate"
+import { DirectSecp256k1HdWallet, coin, coins } from "@cosmjs/proto-signing";
 import Long from "long";
 
 
@@ -56,55 +56,34 @@ async function bridge() {
     });
     
     console.log("After getDepositAddress: ", depositAddress)
-    
-    //need to create osmosis wallet
-
-    //osmojs
-    // const mnemonic = config.secrets.osmosis.osmosisMnemonic;
-    // const chain = chains.find(({ chain_name }) => chain_name === 'osmosis');
-    // const signer = await getOfflineSigner({
-    //     mnemonic,
-    //     chain
-    // });
-    // const client = await getSigningOsmosisClient({
-    //     rpcEndpoint: osmoChainRPC,
-    //     signer // OfflineSigner
-    // });
-    // const { transfer } = ibc.applications.transfer.v1.MessageComposer.withTypeUrl
-  
-    // const msg = transfer({
-    //     typeUrl: "http://url",
-    // });
-
-    // const transfer_fee: StdFee = {
-    //     amount: [
-    //     {
-    //         denom: 'uosmo',
-    //         amount: '864'
-    //     }
-    //     ],
-    //     gas: '86364'
-    // };
-    // const response = await client.signAndBroadcast(depositAddress, [msg], transfer_fee);
 
     //cosmojs
-    const wallet = await Secp256k1HdWallet.fromMnemonic(config.secrets.osmosis.osmosisMnemonic);
+    const chain = chains.find(({ chain_name }) => chain_name === 'osmosis');
+    const wallet = await getOfflineSigner({
+        mnemonic: config.secrets.osmosis.osmosisMnemonic,
+        chain
+    });
+    const accounts = await wallet.getAccounts()
+    console.log("Account: ", accounts[0].address )
+    // const wallet = await DirectSecp256k1HdWallet.fromMnemonic(config.secrets.osmosis.osmosisMnemonic);
     const client2 = await SigningStargateClient.connectWithSigner(
         osmoChainRPC,
-        wallet
+        wallet,
     );
+    console.log("Balance: ", await client2.getBalance(accounts[0].address, token_denom))
+    console.log("Balances: ", await client2.getAllBalances(accounts[0].address))
     const memo = "Cross-chain fun";
     const fee2 = {
-        amount: coins(token_amount, token_denom),
-        gas: "222000", // 222k
+        amount: coins("6000", "uosmo"),
+        gas: "2220000", // 222k
     };
 
     // both timeouts set
     const result = await client2.sendIbcTokens(
         sender_account,
-        depositAddress,
-        coin(amountIn, token_denom),
-        "Axelar",
+        depositAddress, //uusdc
+        coin(amountIn, "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858"),
+        "transfer",
         "channel-208",
         { revisionHeight: Long.fromNumber(123), revisionNumber: Long.fromNumber(456) },
         Math.floor(Date.now() / 1000) + 60,
