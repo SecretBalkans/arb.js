4580bc64878eb9cdcd6a79bc408f7a713f705566
Antonymous <5652120-antonymous@users.noreply.gitlab.com>
2023-06-07 22:20:21 +0300

    fix: added timing cals

--- src/arbitrage/dexArbitrage.ts
+++ src/arbitrage/dexArbitrage.ts
@@ -277,6 +277,8 @@ export class ArbCalculator {
               [reverseBaseAmount, reverseChangedPair, dex1, dex0, height1, height0],
             ];
             await Aigle.forEach(pathArgs, async (args: [Amount, ArbPair, DexProtocol<DexProtocolName>, DexProtocol<DexProtocolName>, number, number]) => {
+              const label = args[1].join('-');
+              console.time(label)
               const arbPath = this.calcDexArbOut(...args);
               if (arbPath.amountOut?.isGreaterThan(arbPath.amountIn)) {
                 const capacityUntilBalance = this.calculateCapacityUntilBalance(arbPath);
@@ -290,6 +292,7 @@ export class ArbCalculator {
                 this.setCurrentCapacity(defaultCapacity);
                 emitter.next(arbPath)
               }
+              console.timeEnd(label)
               await new Promise(resolve => setImmediate(resolve));
             })
           }
